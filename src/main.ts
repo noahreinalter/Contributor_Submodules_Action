3f6816d9ca3da10a9d3f007bf904722e7b4e5c0c
noahreinalter <38698485+noahreinalter@users.noreply.github.com>
2022-01-20 00:25:29 +0100

    Fixed lint errors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as fs from 'fs'
 import * as github from '@actions/github'
-const fs = require('fs')
-const exec = require('@actions/exec')
 
 async function run(): Promise<void> {
   if (!checkIfValidUser()) return
@@ -13,54 +13,44 @@ async function run(): Promise<void> {
 
     const username: string = splitUrl[splitUrl.length - 2]
 
-    await exec.exec('git submodule add ' + url + ' submodules/' + username)
+    await exec.exec(`git submodule add ${url} submodules/${username}`)
 
-    const fileNames: string[] = fs.readdirSync('./submodules/' + username)
+    const fileNames: string[] = fs.readdirSync(`./submodules/${username}`)
 
     core.debug(
-      'There are ' + fileNames.length.toString() + ' files in the new submodule'
+      `There are ${fileNames.length.toString()} files in the new submodule`
     )
 
-    fileNames.forEach((file: string) => {
+    for (const file of fileNames) {
       core.debug(file)
       if (file.match(/^\d*$/) != null) {
         addLink(file, username)
       }
-    })
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
-async function addLink(year: string, username: string) {
-  core.debug('Add ' + year + ' for ' + username)
+async function addLink(year: string, username: string): Promise<void> {
+  core.debug(`Add ${year} for ${username}`)
   if (!fs.existsSync(year)) {
-    core.debug('Create dir ' + year)
+    core.debug(`Create dir ${year}`)
     fs.mkdirSync(year)
   }
   await exec.exec(
-    'ln -s ../submodules/' +
-      username +
-      '/' +
-      year +
-      ' ./' +
-      year +
-      '/' +
-      username
+    `ln -s ../submodules/${username}/${year} ./${year}/${username}`
   )
 }
 
 function checkIfValidUser(): boolean {
-  let isValid: boolean = false
-  core
-    .getInput('users')
-    .split(' ')
-    .forEach((user: string) => {
-      if (user == github.context.actor) {
-        core.debug(user + '==' + github.context.actor)
-        isValid = true
-      }
-    })
+  let isValid = false
+  for (const user of core.getInput('users').split(' ')) {
+    if (user === github.context.actor) {
+      core.debug(`${user}===${github.context.actor}`)
+      isValid = true
+    }
+  }
 
   return isValid
 }
