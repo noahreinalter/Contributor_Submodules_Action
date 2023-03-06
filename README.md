b21179c63d7f5f2eeaa093557c78e80549625ff5
noahreinalter <38698485+noahreinalter@users.noreply.github.com>
2023-03-06 16:00:48 +0100

    Add update functionality and rename reload_submodules

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ A GitHub Action that automates adding submodules
         url: 'url'
         # default: '^\d*$' Regex for the folders to link
         regex: '^\d*$'
-        # default: 'false' Adds links for submodules if they are missing
-        reload_submodules: 'false'
+        # default: 'false' If 'true' adds links for submodules if they are missing
+        relink_submodules: 'false'
+        # default: 'false' If 'true' fetch and update all submodules
+        update_submodules: 'false'
+        # default: 'submodules' Path to the folder where the submodules are saved to
+        submodule_save_location: 'submodules'
 ```
--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,32 @@ import * as fs from 'fs'
 async function run(): Promise<void> {
   try {
     const url: string = core.getInput('url')
-    if (url != null && url.match(/^https:\/\/.*\.git$/)) {
-      await addSubmodule(url)
+    const path: string = core.getInput('submodule_save_location')
+
+    if (core.getInput('update_submodules') === 'true') {
+      await exec.exec('git submodule update --remote')
     }
 
-    if (core.getInput('reload_submodules') === 'true') {
-      reloadAllSubmodules()
+    if (url != null && url.match(/^https:\/\/.*\.git$/)) {
+      await addSubmodule(url, path)
+    }
+
+    if (core.getInput('relink_submodules') === 'true') {
+      reloadAllSubmodules(path)
     }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
-async function addSubmodule(url: string): Promise<void> {
+async function addSubmodule(url: string, path: string): Promise<void> {
   const splitUrl: string[] = url.split('/')
 
   const username: string = splitUrl[splitUrl.length - 2]
 
-  await exec.exec(`git submodule add ${url} submodules/${username}`)
+  await exec.exec(`git submodule add ${url} ${path}/${username}`)
 
-  const fileNames: string[] = fs.readdirSync(`./submodules/${username}`)
+  const fileNames: string[] = fs.readdirSync(`./${path}/${username}`)
 
   core.debug(
     `There are ${fileNames.length.toString()} files in the new submodule`
@@ -35,34 +41,38 @@ async function addSubmodule(url: string): Promise<void> {
   for (const file of fileNames) {
     if (regex.test(file)) {
       core.debug(`Add link for file ${file}`)
-      addLink(file, username)
+      addLink(file, username, path)
     }
   }
 }
 
-async function reloadAllSubmodules(): Promise<void> {
-  const submoduleNames: string[] = fs.readdirSync('./submodules')
+async function reloadAllSubmodules(path: string): Promise<void> {
+  const submoduleNames: string[] = fs.readdirSync(`./${path}`)
 
   for (const submoduleName of submoduleNames) {
-    const fileNames: string[] = fs.readdirSync(`./submodules/${submoduleName}`)
+    const fileNames: string[] = fs.readdirSync(`./${path}/${submoduleName}`)
     const regex = RegExp(core.getInput('regex'))
 
     for (const file of fileNames) {
       if (regex.test(file)) {
-        addLink(file, submoduleName)
+        addLink(file, submoduleName, path)
       }
     }
   }
 }
 
-async function addLink(targedName: string, username: string): Promise<void> {
+async function addLink(
+  targedName: string,
+  username: string,
+  path: string
+): Promise<void> {
   core.debug(`Add ${targedName} for ${username} if necessary`)
   if (!fs.existsSync(targedName)) {
     core.debug(`Create dir ${targedName}`)
     fs.mkdirSync(targedName)
   }
   await exec.exec(
-    `ln -s ../submodules/${username}/${targedName} ./${targedName}/${username}`
+    `ln -s ../${path}/${username}/${targedName} ./${targedName}/${username}`
   )
 }
 
